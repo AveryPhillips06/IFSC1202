7b477fd20b52f8f9660c1f0fe88dda4e492dd549
AveryPhillips06 <aphillips2@ualr.edu>
2023-02-01 15:49:28 +0000

    unit 01.06

--- 01.06 Two Timestamps.py
+++ 01.06 Two Timestamps.py
@@ -1,16 +1,17 @@
-# Read an integer:
-a = int(input(Hour))
+print ("first time stamp")
+a = int(input("Hour:"))
 
 # Read a int:
-b = int(input(Minutes))
+b = int(input("Minutes:"))
 
 # Read a int:
-c = int(input(Seconds))
-d = int(input(hour))
-e = int(input(minutes))
-f = int(input(seconds))
+c = int(input("Seconds:"))
+print ("second time stamp")
+d = int(input("Hour:"))
+e = int(input("Minutes:"))
+f = int(input("Seconds:"))
 # calculating total number of seconds
-seconds = a*3600 + b*60 + c - d*3600 + e*60 + f
+seconds = (d*3600 + e*60 + f) - (a*3600 + b*60 + c)
 
 # printing result
 print(seconds)

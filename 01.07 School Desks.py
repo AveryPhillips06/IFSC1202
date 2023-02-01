6d2f45247e8a46eeae5e4aa047b1ff42742b4976
AveryPhillips06 <aphillips2@ualr.edu>
2023-02-01 06:32:29 +0000

    unit 1 Schooldesk

---
+++ 01.07 School Desks.py
@@ -0,0 +1,22 @@
+def desks(s):
+    if s%2==0:
+        return s//2
+    else:
+        return (s//2)+1
+
+
+
+#taking number of students in each class
+a=int(input())
+b=int(input())
+c=int(input())
+
+
+#calling function
+number_of_desks=0
+number_of_desks=desks(a)
+number_of_desks+=desks(b)
+number_of_desks+=desks(c)
+
+#printing function
+print(number_of_desks)

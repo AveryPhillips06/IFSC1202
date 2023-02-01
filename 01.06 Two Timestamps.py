fc9e9c63b9d62b8870273adf16bd03f25a6c26af
AveryPhillips06 <aphillips2@ualr.edu>
2023-02-01 06:24:49 +0000

    Unit 1-6 Timestamp

--- 01.06 Two Timestamps.py
+++ 01.06 Two Timestamps.py
@@ -6,9 +6,9 @@ b = int(input(Minutes))
 
 # Read a int:
 c = int(input(Seconds))
-d = int(input(Hour 2))
-e = int(input(Minutes 2))
-f = int(input(Seconds))
+d = int(input(hour))
+e = int(input(minutes))
+f = int(input(seconds))
 # calculating total number of seconds
 seconds = a*3600 + b*60 + c - d*3600 + e*60 + f
 

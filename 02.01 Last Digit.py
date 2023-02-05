5981de6160a2f18273d2298e18c3519fe98d80fa
AveryPhillips06 <aphillips2@ualr.edu>
2023-02-05 22:00:06 +0000

    02.01 Last Digit.py

---
+++ 02.01 Last Digit.py
@@ -0,0 +1,8 @@
+#int main()
+{ intNumber,LastDigit; //vociableo declared to store value
+printf ("inenter a number:"); //prompt message on display
+scanf("%d"& Number); //Input of a number
+LastDigit = Number%10;//calculation of LastDigit.
+Printf ("In LastDigit:=%d",LastDigit);//Display result.
+return 0;
+}

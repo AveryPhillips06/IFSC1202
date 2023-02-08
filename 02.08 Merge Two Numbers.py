cef2b289caa652eebc5cf124a01f26dddd6bf822
AveryPhillips06 <aphillips2@ualr.edu>
2023-02-08 14:45:11 +0000

    02.08 Merge Two Numbers.py

--- 02.08 Merge Two Numbers.py
+++ 02.08 Merge Two Numbers.py
@@ -5,9 +5,5 @@ a = int(firstNumber%100//10)
 b = int(firstNumber%10)
 c = int(secondNumber%100//10)
 d = int(secondNumber%10)
-#print(number%10)
-#print(number%100//10)#
-#b = int(number%10)
-#a = int(number%100//10)
-#c = int(number%1000//100)
+
 print(a,c,b,d)

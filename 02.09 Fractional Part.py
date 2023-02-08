433fd52e67d1a28606d095fdcc07d3a05463d9e1
AveryPhillips06 <aphillips2@ualr.edu>
2023-02-08 15:32:55 +0000

    02.09.Fractional Part.py

--- 02.09 Fractional Part.py
+++ 02.09 Fractional Part.py
@@ -1,7 +1,4 @@
-def fractional(num):
-    number = float("0."+str(num).split(".")[1])
-    print(f"Fractional Part: {number}")
-
-
 num = float(input("Enter a Number: "))
-fractional(num)
+whole = int(num)
+frac = num - whole
+print(round (frac,10))

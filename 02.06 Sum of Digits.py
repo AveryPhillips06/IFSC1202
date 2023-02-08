f0fe34dd6bbce4f4b33b3d0c95665af4dd2607ee
AveryPhillips06 <aphillips2@ualr.edu>
2023-02-08 04:22:56 +0000

    02.06 sum of Digits.py

--- 02.06 Sum of Digits.py
+++ 02.06 Sum of Digits.py
@@ -1,7 +1,7 @@
-number = int(input("Enter a 3 Digit Number:"))#take int type input
-add = 0 #initialize the add variable which stores the sum to 0
-for i in range(0,3):#loop through 0 to 3 i.e. 3 digits
-add += number%10#find the modulo so that you get the last digit of the 3 digit number
-number = number//10#divide the by 10 so that you get the remaining digits
-
-print("Sum of Digits: {}".format(add))#print the sum of digits using the format
+number = int(input("Enter a number:"))
+#print(number%10)
+#print(number%100//10)
+b = int(number%10)
+a = int(number%100//10)
+c = int(number%1000//10)
+print(b+a+c)

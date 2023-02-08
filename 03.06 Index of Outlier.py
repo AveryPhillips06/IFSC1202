9b804464b054b609ab4f602b6df88c80dcd5ecf4
AveryPhillips06 <aphillips2@ualr.edu>
2023-02-08 11:59:27 +0000

    03.07 Leap Year.py

---
+++ 03.06 Index of Outlier.py
@@ -0,0 +1,21 @@
+#include <iostream>
+#using namespace std;
+int main()
+{
+int a,b,c;
+
+cout<<"Enter First Number: ";
+cin>>a;
+cout<<"\nEnter second number: ";
+cin>>b;
+cout<<"\nEnter Third number: ";
+cin>>c;
+
+if((a==b) && (a!=c))
+cout<<"3";
+else if:((a==c) && (a!=b))
+cout<<"2";
+else:
+cout<<"1";
+return 0;
+}

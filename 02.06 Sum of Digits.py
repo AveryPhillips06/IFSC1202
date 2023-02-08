2bcbfb188c6f307aba7c2cf77ca15cc67ac43761
AveryPhillips06 <aphillips2@ualr.edu>
2023-02-08 15:25:18 +0000

    02.06 Sum of Digits.py

--- 02.06 Sum of Digits.py
+++ 02.06 Sum of Digits.py
@@ -1,7 +1,13 @@
 number = int(input("Enter a number:"))
 #print(number%10)
 #print(number%100//10)
-b = int(number%10)
-a = int(number%100//10)
-c = int(number%1000//10)
-print(b+a+c)
+units = number % 10
+number = number // 10
+tens = number % 10
+hundreds = number // 10
+print(units + tens + hundreds)
+
+
+
+
+

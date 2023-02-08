91cfe4c3b4e3334206f3b729d5353bc5a941b761
AveryPhillips06 <aphillips2@ualr.edu>
2023-02-08 05:04:00 +0000

    02.08 Merge Two Numbers.py

--- 02.08 Merge Two Numbers.py
+++ 02.08 Merge Two Numbers.py
@@ -1,11 +1,13 @@
-#Statement to import array
-import array as array
 #get inputs from user
 firstNumber=int(input("Enter First 2 Digit Number:"))
 secondNumber=int(input("Enter Second 2 Digit Number:"))
-#Convert the individual elements into array
-a=arr.array('i',[int(firstNumber/10),int(secondNumber/10),int(firstNumber%10),int(secondNumber%10)]);
-#merge array to string
-merged=''.join(map(str,a));
-#print the result
-print("Merged Number:",merged)
+a = int(firstNumber%100//10)
+b = int(firstNumber%10)
+c = int(secondNumber%100//10)
+d = int(secondNumber%10)
+#print(number%10)
+#print(number%100//10)#
+#b = int(number%10)
+#a = int(number%100//10)
+#c = int(number%1000//100)
+print(a,c,b,d)

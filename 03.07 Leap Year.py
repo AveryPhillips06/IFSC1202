305d21db5a51c6715c0408a16e938a63049ee018
AveryPhillips06 <aphillips2@ualr.edu>
2023-02-08 18:56:03 +0000

    03.07 Leap Year.py

--- 03.07 Leap Year.py
+++ 03.07 Leap Year.py
@@ -1,13 +1,9 @@
-#defining a function
+
 def LeapOrNot(n):
-    #checking conditions for leap year
     if n%400==0 or n%100!=0 and n%4==0:
         print("LEAP YEAR")
     else:
-        #if not leap year then its a normal year
         print("COMMON YEAR")
 
-#taking user input
 n=int(input("Enter year:\n"))
-#passing input onto function
 LeapOrNot(n)

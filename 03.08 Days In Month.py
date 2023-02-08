578c0bf9a0e1aba5e71bae309f17aa098e508e25
AveryPhillips06 <aphillips2@ualr.edu>
2023-02-08 16:56:50 +0000

    03.08 Days in month

--- 03.08 Days In Month.py
+++ 03.08 Days In Month.py
@@ -3,12 +3,10 @@ def getDaysInMonth(month, year):
     month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
     if month < 1 and month > 12:
         return -1
-    if isLeapYear(year):
+    if LeapYear(year):
         if month == 2:
             return 29
         else:
             return 28
     return month_days[month-1]
 
-print(getDaysInMonth(2, 2014))
-print(getDaysInMonth(2, 2020))

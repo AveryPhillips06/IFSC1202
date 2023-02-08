52a8f31b76507a7a2a5de9f837687f10ff078fa1
AveryPhillips06 <aphillips2@ualr.edu>
2023-02-08 02:42:52 +0000

    unit 02.01

--- 02.01 Last Digit.py
+++ 02.01 Last Digit.py
@@ -1,8 +1,5 @@
-#int main()
-{ intNumber,LastDigit; //vociableo declared to store value
-printf ("inenter a number:"); //prompt message on display
-scanf("%d"& Number); //Input of a number
-LastDigit = Number%10;//calculation of LastDigit.
-Printf ("In LastDigit:=%d",LastDigit);//Display result.
-return 0;
-}
+#Take a number as input
+number = int(input("Enter a number:"))
+
+#Get remainder of the number when mod by 10
+print(number%10)

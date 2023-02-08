53d01ad3f8b408f5d3e17af7c4e8a36871f90402
AveryPhillips06 <aphillips2@ualr.edu>
2023-02-08 16:14:21 +0000

    03.02 Equal Number.py

--- 03.02 Equal Numbers.py
+++ 03.02 Equal Numbers.py
@@ -1,13 +1,12 @@
-a=int(input('Enter 1st number'))
-b=int(input('Enter 2nd number'))
-c=int(input('Enter 3rd number'))
+a=int(input("Enter 1st number:"))
+b=int(input("Enter 2nd number:"))
+c=int(input("Enter 3rd number:"))
 if(a==b==c):
+    print(3)
 
-print('3')
+elif ((a==b and a!=c) or (b==c and c!=a) or (a==c and c!=b)):
 
-elif((a==b and a!=c) or (b==c and c!=a) or (a==c and c!=b)):
-
-print('2')
-elif(a!=b!=c):
-print ('0')
+    print(2)
+elif (a!=b!=c):
+    print (0)
 

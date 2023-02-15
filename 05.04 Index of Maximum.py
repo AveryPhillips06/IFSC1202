b964e74493d29e4572dfed789a0ff50cc769e608
AveryPhillips06 <aphillips2@ualr.edu>
2023-02-15 21:32:16 +0000

    05.04 Index of Maximum.py

---
+++ 05.04 Index of Maximum.py
@@ -0,0 +1,12 @@
+highest = 0
+index = 0
+highest_index =0
+while True:
+        num = int(input('Enter a Number (zero to quit):'))
+        if (num==0): break
+        index+=1
+        if num>highest:
+            highest,highest_index=num,index
+
+print('Maximun:{}'.format (highest))
+print ('Index of Maximum: {}'.format(highest_index))

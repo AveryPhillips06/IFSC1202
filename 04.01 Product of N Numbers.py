60a425f87793c13cef8849ca852bf042b6ee19da
AveryPhillips06 <aphillips2@ualr.edu>
2023-02-15 14:40:09 +0000

    04.01 Projuct of N Number.py

--- 04.01 Product of N Numbers.py
+++ 04.01 Product of N Numbers.py
@@ -2,7 +2,7 @@ n=input('Enter the count of Numbers:');
 n=int(n)
 m=1;
 for i in range(n)
-    k=input('Enter Numnber:');
-    K=int("k")
-    m=m*k;
+k=input('Enter Numnber:');
+K=int("k")
+m=m*k;
 print ('product:{})'.format(m)):

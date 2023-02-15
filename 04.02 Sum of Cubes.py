5f022649aa975afacec2a1d35be87a4147b90099
AveryPhillips06 <aphillips2@ualr.edu>
2023-02-15 20:14:12 +0000

    04.02 Sum of Cubes.py

--- 04.02 Sum of Cubes.py
+++ 04.02 Sum of Cubes.py
@@ -1,2 +1,17 @@
-name = input('name:')
-print("Hello", name)
+N = 5
+
+# initializing k to one
+k = 1
+
+# initializing total to zero
+total = 0
+
+# looping from 1 to N
+while k <= N:
+    # updating the value of total
+    total = total + (k * k * k)
+
+    # incrementing the value of k by 1
+    k = k + 1
+
+print(total)

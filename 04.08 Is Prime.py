860727f34da45062d39c1b8c447c272c36f5b9ad
AveryPhillips06 <aphillips2@ualr.edu>
2023-02-15 21:11:13 +0000

    04.08Prime.py

--- 04.08 Is Prime.py
+++ 04.08 Is Prime.py
@@ -1,15 +1,11 @@
-def primeno(s,e): #function start
-    for i in range(s, e+1): #for loop start for given interval
-        if i>1:
-for k in range(2,i): #for the given no if any no which devide it or not
-    if(i % k==0):
-        break #break statemet
+N = int(input("Enter N: "))
+flag = True
+if N > 1:
+    for i in range(2,int((N/2))+1):
+        if N%i == 0:
+            print("COMPOSITE")
+            break
     else:
-        print(i) #printing the prime no
-
-#function block end
-
-if __name__ == "__main__": #main
-    start=int(input()) #user input1 for starting interval
-    end=int(input()) #user input2 for end interval
-    primeno(start,end) #calling function
+        print("PRIME")
+else:
+    print("COMPOSITE")

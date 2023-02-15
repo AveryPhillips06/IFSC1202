e63ba8577dce2e7c2afbf2998d4adcf48892e37a
AveryPhillips06 <aphillips2@ualr.edu>
2023-02-15 20:57:53 +0000

    04.03 Product of N Numbers.py

--- 04.03 Product of N Numbers.py
+++ 04.03 Product of N Numbers.py
@@ -1,13 +1,5 @@
-n=int(input("Enter n: "))
-# sum to store factorial of numbers from 1 to n
-sum=0
-# store factorial
-fact=1
-# i will run from 1 t n
-for i in range(1,n+1):
-  # multiply previously calculated fact with i
-  fact=fact*i
-  # add ne
-  sum=sum+fact
-# print sum
-print("sum = ",sum)
+number = int(input("Enter Number: "))
+fact = 1
+for i in range(1,number+1):
+    fact = fact*i
+print(fact)

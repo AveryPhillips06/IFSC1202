0886161739b7689626f934b358b326a78d2cd714
AveryPhillips06 <aphillips2@ualr.edu>
2023-02-15 22:04:04 +0000

    05.06 Elementsd Greater Then Previous.py

--- 05.06 Elements Greater Than Previous.py
+++ 05.06 Elements Greater Than Previous.py
@@ -1,14 +1,16 @@
-largest = 0
-occurrences = 0
-firstNumber = True
-while True:
-    num = int(input('Enter a Number (zero to quit): '))
-    if num == 0: break
-    if firstNumber or largest < num:
-        largest = num
-        occurrences = 1
-    elif largest == num:
-        occurrences += 1
-    firstNumber = False
-print('Maximum:', largest)
-print('Number of Occurrences:', occurrences)
+num = 1
+previous = 0
+i = 0
+count = 0
+
+while num != 0:
+    num = int(input("Enter a Number (zero to quit): "))
+    if num != 0:
+        if i == 0:
+            previous = num
+        if num > previous:
+            count += 1
+        i += 1
+        previous = num
+
+print("Number of Values Greater Than the Previous:", count)

8efe66a704289d72f8d18e6cd3d7cc08e6dd7bf3
AveryPhillips06 <aphillips2@ualr.edu>
2023-02-15 22:15:30 +0000

    05.09 Fibonacci Number.py

--- 05.09 Fibonacci Numbers.py
+++ 05.09 Fibonacci Numbers.py
@@ -4,14 +4,14 @@ def Fibonacci(n):
 
         return n
 
-if(n==0):
-    return 0
+    if(n==0):
+        return 0
 
-if (n == 1 or n == 2): 
+    if (n == 1 or n == 2): 
 
-    return 1
+        return 1
 
-return ((Fibonacci(n-1)) + (Fibonacci(n-2))) 
+    return ((Fibonacci(n-1)) + (Fibonacci(n-2))) 
 
 
 n = int(input("Enter Fibonacci Sequesnce Number: "))

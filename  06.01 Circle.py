b2ea103c22e8e36c297ce54f74d66b8cd5c4a9e6
AveryPhillips06 <aphillips2@ualr.edu>
2023-02-22 16:10:09 +0000

    06.01 Circle.py

---  06.01 Circle.py
+++  06.01 Circle.py
@@ -1,34 +1,18 @@
-import math
+from math import pi
 def diameter(radius):
     return radius * 2
 
 def circumference(radius):
-    return 2 * math.pi * radius
+    return 2 * pi * radius
 
 def area(radius):
-    return math.pi * radius * radius
+    return pi * radius * radius
+print("{:>15s} {:>15s} {:>15s} {:>15s}".format("Radius","Diameter","Circumference","Area"))
+file1 = open("06.01 Radius.txt")
+line = file1.readline() 
+while line != '':
+    radius = float(line)
+    print("{:>15.5f} {:>15.5f} {:>15.5f} {:>15.5f}".format(radius,diameter(radius),circumference(radius),area(radius)))
+    line = file1.readline() 
 
-
-
-def main():
-
-
-    file1 = open("06.01 Radius.txt","r")
-
-    lst = []
-
-    while True:
-        line = file1.readline()
-        if not line:
-            break
-        else:
-            lst.append(float(line))
-
-    print("{:>15} {:>15} {:>15} {:>15}".format("Radius","Diameter","Circumference","Area"))
-    for radius in lst:
-        print("{:>15.5f} {:>15.5f} {:>15.5f} {:>15.5f}".format(radius,diameter(radius),circumference(radius),area(radius)))
-
-
-
-if __name__ == "__main__":
-    main()
+file1.close()

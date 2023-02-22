7ffa2fc52a044a2d2023007f99f0598a45b7d778
AveryPhillips06 <aphillips2@ualr.edu>
2023-02-22 16:44:35 +0000

    06.05CompareFile.py

--- 06.05 Compare Files.py
+++ 06.05 Compare Files.py
@@ -1,11 +1,8 @@
-#open files
-f1 = open("CompareFileA.txt")
-f2 = open("CompareFileB.txt")
-#make lists to store lines
+
+f1 = open("06.05CompareFileA.txt")
+f2 = open("06.05CompareFileB.txt")
 lst1 = [] 
 lst2 = []
-
-#store lines 
 for l in f1:
     lst1.append(l)
     
@@ -13,16 +10,13 @@ for l in f2:
     lst2.append(l)
     
 cnt=0 #counter
-#loop to compare lines
 for i in range(len(lst1)):
     if(lst1[i]!=lst2[i]):
         print(f"Line :{i+1} - {lst1[i]}")
         print(f"Line :{i+1} - {lst2[i]}")
         cnt+=1
         
-#print differences
 print(cnt,"differences")
     
-#close files     
 f1.close()
 f2.close()

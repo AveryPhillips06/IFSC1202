9d49cdd3d107fc67156b89e70de44ac9719f169e
AveryPhillips06 <aphillips2@ualr.edu>
2023-03-01 15:52:38 +0000

    06.06 Split Numbers.py

--- 06.06 Split Numbers.py
+++ 06.06 Split Numbers.py
@@ -1,36 +1,42 @@
-def isEven(number):
-    return number %2 ==0
-
-def isOdd(number):
-    return number %2 ==1
-
-def isPrime(number):
-#number should be greater than 1
-    if(number> 1):
-#check for any factor exist till half of the number
-        for index in range(2,number//2):
-#factor exist then return false
-            if(number % index == 0):
+def isEven(n):
+    return (n%2==0)
+def isOdd(n):
+    return (n%2)
+def isPrime(n):
+    if(n > 1):
+        for i in range(2,n//2):
+            if(n%i==0):
                 return False
-        return True
+        else:
+            return True
     else:
         return False
-
-
-#main driver program
-inputFile = open("06.06 Numbers.txt")
-outputEven = open("6.6 Evennumbers.txt","w+")
-outputOdd = open("6.6 Oddnumbers.txt","w+")
-outputprime = open("6.6 Primenumbers.txt","w+")
-
-#iterate over lines in n umbers.txt
-for line in inputFile:
-    line = int(line.strip())
-#if even write in even file
-    if isEven(line):
-        outputEven.write(str(line) + "\n")
-    elif isOdd(line): #if odd write in odd file , i just used it but not required only else will also work
-        outputOdd.write(str(line) + "\n")
-
-if isPrime(line):
-    outputprime.write(str(line) + "\n")
+file1 = open('06.06 Numbers.txt','r')
+file2 = open('6.6 Evennumbers.txt','w')
+file3 = open('6.6 Oddnumbers.txt','w')
+file4 = open('6.6 Primenumbers.txt','w')
+nums = file1.readlines()
+evencount = 0
+oddcount = 0
+primecount = 0
+readcount = 0
+for line in nums:
+    n = int(line.strip())
+    if(isEven(n)):
+        file2.write(str(n)+"\n")
+        evencount += 1
+    elif(isOdd(n)):
+        file3.write(str(n)+"\n")
+        oddcount += 1
+    if(isPrime(n)):
+        file4.write(str(n)+"\n")
+        primecount += 1
+readcount += 1
+print("{} even numbers".format(evencount))
+print("{} odd numbers".format(oddcount))
+print("{} prime numbers".format(primecount))
+print("{} read numbers".format(readcount))
+file1.close()
+file2.close()
+file3.close()
+file4.close()

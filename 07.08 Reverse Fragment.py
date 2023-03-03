7a4c4353d8edfe6a6427c7938d9856c48e317f3d
AveryPhillips06 <aphillips2@ualr.edu>
2023-03-03 17:52:42 +0000

    07.08 Reverse Fragment.py

--- 07.08 Reverse Fragment.py
+++ 07.08 Reverse Fragment.py
@@ -1,14 +1,5 @@
-s= "abch123h456hdef" //you can use input to take input but i have use predefined strring for help here
-a=s.find('h') //this finds if there is any h from begenning indexing from 0
-a=a+1 //to increment a with 1 you'll understand why
-b=s.rfind('h') // this finds if any h from beckward
-if(a==b): //if there is only h or none is there then both index will be same and string need to reverse
-print(a) //prints a
-else:       
- print(s[:a],end="") this prints upto frist h found
-t=s[a:b] //this provide a substring of s from a to b;
-print(t[::-1],end="") //this prints given substring t in reverse order (-1)
-print(s[b:],end="") //this prints rest of the string
-//and you get the required resultn i.e abch654h321hdef
 
-//please don't mess with indentation it will cause issues
+string = input("Enter a string: ")
+h_index = [i for i in range(len(string)) if string[i] == 'h']
+h_0, h_1 = h_index[0], h_index[-1]
+print(string[h_0:h_1+1][::-1])

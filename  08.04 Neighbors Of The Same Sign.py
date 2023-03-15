292b1445e4b6b6172eebf6d064964f5ef41efe3f
AveryPhillips06 <aphillips2@ualr.edu>
2023-03-15 17:48:22 +0000

    08.04 Neighbors Of The Same Sign.py

---  08.04 Neighbors Of The Same Sign.py
+++  08.04 Neighbors Of The Same Sign.py
@@ -1,7 +1,8 @@
-
-for i in range(len(num_list)-1):
-    if (num_list[i] >= 0 and num_list[i+1] >= 0) or (num_list[i] < 0 and num_list[i+1] < 0):
-        print(f"The first adjacent elements with the same sign are {num_list[i]} and {num_list[i+1]}.")
-        break
+num_string = input("Enter Values Separated by Spaces: ")
+num_list = [int(num) for num in num_string.split()]
+for i in range(len(num_list) - 1):
+      if (num_list[i] >= 0 and num_list[i+1] >= 0) or (num_list[i] < 0 and num_list[i+1] < 0):
+           print(num_list[i], num_list[i+1])
+           break
 else:
-    print("There are no adjacent elements with the same sign.")
+       print(" ")

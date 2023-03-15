d5e0c1f34997d150b5c8232eb9e0059c8ec77e84
AveryPhillips06 <aphillips2@ualr.edu>
2023-03-15 14:07:38 +0000

    08.01 Odd Indices.py

--- 08.01 Odd Indices.py
+++ 08.01 Odd Indices.py
@@ -1,14 +1,7 @@
-# Prompt for input
 input_str = input("Enter Values Separated by Spaces: ")
-
-# Split input string into a list of strings
 input_list = input_str.split()
-
-# Convert list of strings to list of integers
 int_list = []
 for i in range(len(input_list)):
     int_list.append(int(input_list[i]))
-
-# Print values with odd index number
 for i in range(1, len(int_list), 2):
     print(int_list[i])

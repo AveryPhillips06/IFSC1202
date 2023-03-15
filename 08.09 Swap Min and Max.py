3e00731d6f40896687bec4f2ea8d719d3206d75d
AveryPhillips06 <aphillips2@ualr.edu>
2023-03-15 14:49:01 +0000

    08.09 Swap Min and Max.py

--- 08.09 Swap Min and Max.py
+++ 08.09 Swap Min and Max.py
@@ -1,18 +1,19 @@
+min = 0
+max = 0 
 x = input('enter the values seperated bye spaces:  ')
-list = x.split(" ")
-max = list [0]
-l=len(list)
-for i in range(1):
-    if list [i]>max:
-        max = list [i]
-x = list.index(max)
-min = list [0]
-for i in range(l):
-    if list [i]<min:
-        min = list [i]
-y = list.index (min)
-list [x] = min
-list [y] = max
-final = ' '.join(list)
-print("swap minimum and maximum:  ".final)
+list = x.split()
+for i in range(1,len(list)):
+    if int(list[i]) > int(list[max]):
+        max = i
+    if int(list[i]) < int(list[min]):
+        min = i
+temp = list[min]
+list[min] = list[max]
+list[max] = temp
+print("swap minimum and maximum:  ",end = " ")
+for i in range(len(list)):
+    print(list[i], sep= " ", end = " ")
+print()
+
+
 

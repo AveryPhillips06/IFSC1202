28ca8febcf0fc29d89b67ed60ba77e74f1a97d0b
AveryPhillips06 <aphillips2@ualr.edu>
2023-03-29 18:05:20 +0000

    09.04 Unit Conversion.py

--- 09.04 Unit Conversion.py
+++ 09.04 Unit Conversion.py
@@ -1,41 +1,62 @@
+fromValue = int(input("Enter From Value : "))
+fromUnit = input("Enter From Unit(in,ft,yd,mi) : ")
+toUnit = input("Enter To Unit(in,ft,yd,mi) : ")
 
-file = open(r"09.04 Conversion.txt","r")
+floatfromValue = float(fromValue)
 
-table = file.readlines()
+if fromUnit == toUnit:
+    print(str(floatfromValue) + " " + fromUnit + " => " + str(floatfromValue) + " " + fromUnit)
 
-for l in range(len(table)):
-    table[l] = table[l].split(" ")
-    if(l!=1):
-        for e in table[l][1:]:
-            e = float(e)
+elif fromUnit == "in" and toUnit == "ft":
+    toValue = round(fromValue / 12, 7)
+    print(str(floatfromValue) + " " + fromUnit + " => " + str(toValue) + " " + toUnit)
 
-table[0].insert(0,None)
-from_val = float(input("Enter From Value:"))
-from_unit = input("Enter From Unit:")
-to_unit = input("Enter To Unit:")
-flag = 0#The flag is used to detect whether the element was found in the list or not.
+elif fromUnit == "in" and toUnit == "yd":
+    toValue = round(fromValue / 36, 7)
+    print(str(floatfromValue) + " " + fromUnit + " => " + str(toValue) + " " + toUnit)
 
-from_unit_index = 0
-to_unit_index = 0
+elif fromUnit == "in" and toUnit == "mi":
+    toValue = round(fromValue / 63360, 7)
+    print(str(floatfromValue) + " " + fromUnit + " => " + str(toValue) + " " + toUnit)
 
+elif fromUnit == "ft" and toUnit == "in":
+    toValue = round(fromValue * 12, 7)
+    print(str(floatfromValue) + " " + fromUnit + " => " + str(toValue) + " " + toUnit)
 
-for i in range(1,len(table)):
-    if(table[i][0]==from_unit):
-        from_unit_index = i
-        flag = 1
-        break
-if(flag==1):
-    flag = 0#resetting the flag
-    for i in range(1,len(table[0])):
-        if(table[0][i]==to_unit):
-            to_unit_index = i
-            flag = 1
-            break
-    if(flag==1):
-        ans = table[from_unit_index][to_unit_index]*from_val
-        ans = round(ans,7)
-        print(ans)
+elif fromUnit == "ft" and toUnit == "yd":
+    toValue = round(fromValue / 3, 7)
+    print(str(floatfromValue) + " " + fromUnit + " => " + str(toValue) + " " + toUnit)
+
+elif fromUnit == "ft" and toUnit == "mi":
+    toValue = round(fromValue / 5280, 7)
+    print(str(floatfromValue) + " " + fromUnit + " => " + str(toValue) + " " + toUnit)
+
+elif fromUnit == "yd" and toUnit == "in":
+    toValue = round(fromValue * 36, 7)
+    print(str(floatfromValue) + " " + fromUnit + " => " + str(toValue) + " " + toUnit)
+
+elif fromUnit == "yd" and toUnit == "ft":
+    toValue = round(fromValue * 3, 7)
+    print(str(floatfromValue) + " " + fromUnit + " => " + str(toValue) + " " + toUnit)
+
+elif fromUnit == "yd" and toUnit == "mi":
+    toValue = round(fromValue / 1760, 7)
+    print(str(floatfromValue) + " " + fromUnit + " => " + str(toValue) + " " + toUnit)
+
+elif fromUnit == "mi" and toUnit == "in":
+    toValue = round(fromValue * 63360, 7)
+    print(str(floatfromValue) + " " + fromUnit + " => " + str(toValue) + " " + toUnit)
+
+elif fromUnit == "mi" and toUnit == "ft":
+    toValue = round(fromValue * 5280, 7)
+    print(str(floatfromValue) + " " + fromUnit + " => " + str(toValue) + " " + toUnit)
+
+elif fromUnit == "mi" and toUnit == "yd":
+    toValue = round(fromValue * 1760, 7)
+    print(str(floatfromValue) + " " + fromUnit + " => " + str(toValue) + " " + toUnit)
+
+else:
+    if (("in" not in fromUnit) and ("ft" not in fromUnit) and ("yd" not in fromUnit) and ("mi" not in fromUnit)):
+        print("FromUnit is not valid")
     else:
         print("ToUnit is not valid")
-else:
-    print("FromIndex is not valid")

5fde4976811e32bfa0d17a4680dda455cdb3fa5d
AveryPhillips06 <aphillips2@ualr.edu>
2023-03-29 18:27:03 +0000

    09.01 Maximim Of A Two Dimensional List.py

--- 09.01 Maximim Of A Two Dimensional List.py
+++ 09.01 Maximim Of A Two Dimensional List.py
@@ -1,29 +1,23 @@
 import numpy as np;
-# input for rows and columns
-print("Enter the number of rows and columns:")
+input("Enter the number of rows and columns:")
 m = int(input());
 n = int(input());
-# matrix
 matrix = [];
-# take input for matrix
 for i in range(m):          
     l = [];
     print("Enter a line of data:");
     for j in range(n):      
         l.append(int(input()));
-    matrix.append(l);
-# print matrix    
+    matrix.append(l);   
 for i in range(m):          
     for j in range(n):      
         print(matrix[i][j],end = " ");
     print();
-# find max in matrix
 max = 0;
 for i in range(m):          
     for j in range(n):  
         if max < matrix[i][j]:
             max = matrix[i][j];
-# print max
 a = 0;
 b = 0;
 for i in range(m):          

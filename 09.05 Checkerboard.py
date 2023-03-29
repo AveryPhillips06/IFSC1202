af49294d846cd00086135281d7b0d8c3147e47aa
AveryPhillips06 <aphillips2@ualr.edu>
2023-03-29 17:47:54 +0000

    09.05 Checkerboard.py

--- 09.05 Checkerboard.py
+++ 09.05 Checkerboard.py
@@ -1,8 +1,6 @@
 def find_row(size):
     first_row=[]
     for i in range(size):
-    #checking for first row or last row 
-    #then we need to add +  and - 
         if i==0 or i==size-1:
             first_row.append('+')
         else:
@@ -12,32 +10,32 @@ def find_row(size):
 n=int(input("Enter Size :"))
 size=n+2
 l=[]
-#attaching our firs row to the list 
+
 l.append(find_row(size))
-#we have done with first and last row 
-#do the middle part 1 to size-1
+
+
 for i in range(1,size-1):
     column=[]                
     for j in range(size):
         if j==0 or j==size-1:
             column.append('|')
         else:
-            if(i%2==0):              #row is even 
+            if(i%2==0):               
                 if(j%2==0):
                     column.append(' ')
                 else:
                     column.append('*')
-            else:                     #row  is odd
+            else:                     
                 if(j%2==0):
                     column.append('*')
                 else:
                     column.append(' ')   
                     
-    l.append(column)   ##append each row in a after each iteration
-#call find row that is same as first_row and append to list to last row 
+    l.append(column)  
+
 l.append(find_row(size))  
 
- #now list has everthing so convert the list to pattern 
+ 
 o=""
 for i in range(size):
     o+=''.join(l[i])        

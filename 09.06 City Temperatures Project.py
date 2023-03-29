1e7b9db19e777f9684880c2d5da2eb8f87c77b7f
AveryPhillips06 <aphillips2@ualr.edu>
2023-03-29 17:51:36 +0000

    09.06 City Temperatures Project.py

--- 09.06 City Temperatures Project.py
+++ 09.06 City Temperatures Project.py
@@ -1,21 +1,21 @@
 
 with open("09.06 CityTemps.txt", 'r') as file:
-    temps = []  # initializing temps list to store data
+    temps = []  
 
     line = file.readline()
     while line:
         row = line.split()
-        temps.append(row)  # appending list to temps list
-        line = file.readline()   # read next line
+        temps.append(row)  
+        line = file.readline()   
 
 for i in range(len(temps)):
     total = 0    
 
     for temp in temps[i][1:]:
-        total = total + float(temp)   # adding temperature to total
+        total = total + float(temp)   
 
     average = total / (len(temps[i]) - 1)
-    temps[i].append(int(average))  # appending average to temps list
+    temps[i].append(int(average))  
 print(f"{'City' : <10}{'Mo' : <10}{'Tu' : <10}{'We' : <10}{'Th' : <10}{'Fr' : <10}{'Sa' : <10}{'Su' : <10}{'Avg' : <10}")
   
 for i in range(len(temps)):

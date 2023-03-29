a7bb0c99bd27377a24c233680672301edbb425c8
AveryPhillips06 <aphillips2@ualr.edu>
2023-03-29 17:42:26 +0000

    09.03 Scale A Two Dimensional List.py

--- 09.03 Scale A Two Dimensional List.py
+++ 09.03 Scale A Two Dimensional List.py
@@ -1,19 +1,19 @@
 def print_list(a):
     for row in a:
         for elem in row:
-            print(elem, end=' ')#prints space after each element
-        print()#newline
+            print(elem, end=' ')
+        print()
 def scale_list(a,s):
     for i in range(len(a)):
         for j in range(len(a[i])):
-            a[i][j]=s*a[i][j]#multiply scalar with the element
+            a[i][j]=s*a[i][j]
     return a
 ins = open( "09.03 NumbersList.txt", "r" )
 a = []
 for line in ins:
-    number_strings = line.split() # Split the line on runs of whitespace
-    numbers = [int(n) for n in number_strings] # Convert to integers
-    a.append(numbers) # Add the "row" to your list.
+    number_strings = line.split() 
+    numbers = [int(n) for n in number_strings] 
+    a.append(numbers) 
 print_list(a)
 s=int(input("Enter the scalar value : "))
 a=scale_list(a,s)

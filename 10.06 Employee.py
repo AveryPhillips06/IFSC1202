9522444a324c81769b1c9e6b6ea39301bb311f33
AveryPhillips06 <aphillips2@ualr.edu>
2023-04-12 18:25:54 +0000

    10.06 Employee.py

--- 10.06 Employee.py
+++ 10.06 Employee.py
@@ -1,40 +1,58 @@
 class Employee:
-    # create class initializer(constructor) 
-    def __init__(self, FirstName= "", LastName="", IDNumber = 0, HoursWorked=0, Wage=0):
-        # initialize class variables by values passed through constructor 
+   
+    
+    def __init__(self, FirstName, LastName, IDNumber, HoursWorked, Wage):
         self.FirstName = FirstName
         self.LastName = LastName
         self.IDNumber = IDNumber
         self.HoursWorked = HoursWorked
-        self.Wage = Wage 
-
-    # define method WeeklyPay() to calculate weekly pay
+        self.Wage = Wage
+    
+    
     def WeeklyPay(self):
-        weekly_pay = 0
-        # wage is 1 times the wage for 0-40 hours 
-        if(self.HoursWorked<=40):
-            weekly_pay = self.HoursWorked*self.Wage
-        else: 
-        # wage is 1 times the wage for 0 - 40 hours and 1.5 time wage for greater than 40 hours 
-            weekly_pay = (self.Wage*40)+(self.Wage*1.5*(self.HoursWorked-40))
-        return weekly_pay
+        if self.HoursWorked <=  40:
+            totalPay = self.HoursWorked * self.Wage
+        else:
+            basicPay = self.Wage * 40
+            overTime = (self.HoursWorked - 40) * self.Wage * 1.5
+            totalPay = basicPay + overTime
+        return totalPay
 
-    ## define __str__ method to print object details
-    def __str__(self):
-        return self.FirstName+" "+self.LastName+" "+str(self.IDNumber)+" "+str(self.HoursWorked)+" "+str(self.Wage)+" "+str(self.WeeklyPay())
+
+data_file = open("10.06 Payroll.txt")
+
+
+print(f"{'First':>5}{'Last':>8}{'ID':>8}{'Hours':>8}{'Hourly':>8}{'Weekly':>8}")
+print(f"{'Name':>5}{'Name':>8}{'Number':>8}{'Worked':>8}{'Wage':>8}{'Pay':>8}")
+
+
+for line in data_file:
+
+   
+    line = line.strip()
+    
+    if line != '':
 
         
-## create main function to implement all functionality
-if "__main__"== __name__:
-    # create file pointer to read file 10.06 Payroll.txt
-    file = open("10.06 Payroll.txt")
-    # read file line by line 
-    # print heading of output
-    print("First\tLast\tId\tHours\tHourly\tWeekly")
-    print("Name\tName\tNumber\tWorked\tWage\tPay")
-    for line in file.readlines():
-        line = line.split(",")
-        # create Employee class object based details read from file
-        emp = Employee(line[0], line[1], int(line[2]), int(line[3]), float(line[4]))
-        # print the employee details with weekly pay
-        print("{}\t{}\t{}\t{}\t{}\t{:.2f}".format(emp.FirstName, emp.LastName, emp.IDNumber, emp.HoursWorked, emp.Wage, emp.WeeklyPay()))
+        line = line.split(', ')
+
+       
+        firstName = line[0]
+        lastName = line[1]
+        idNumber = line[2]
+        hoursWorked = float(line[3])
+        wage = float(line[4])
+
+        emp = Employee(firstName, lastName, idNumber, hoursWorked, wage)
+
+        fName = emp.FirstName
+        lName = emp.LastName
+        idNum = emp.IDNumber
+        hours = emp.HoursWorked
+        wage = emp.Wage
+
+        pay = emp.WeeklyPay()
+        
+        print(f"{fName:>5}{lName:>8}{idNum:>8}{hours:>8.2f}{wage:>8.2f}{pay:>8.2f}")
+
+data_file.close()
